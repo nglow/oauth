94732edfc43de1e4d72f79f4e3bab9db64f40f0f
nglow <nglow90@gmail.com>
2021-11-29 23:26:38 +0900

    Complete CRUD except Oauth

---
+++ src/main/java/com/example/oauth/web/IndexController.java
@@ -0,0 +1,39 @@
+package com.example.oauth.web;
+
+import com.example.oauth.service.posts.PostsService;
+import com.example.oauth.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequiredArgsConstructor
+public class IndexController {
+
+    private final PostsService postsService;
+    private final HttpSession httpSession;
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        return "index";
+    }
+
+    @GetMapping("/posts/{id}/update")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+}
